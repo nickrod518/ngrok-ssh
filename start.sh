7a9c7c1dd6e04856b29a719bb8e954107f4bbb5b
Nick Rodriguez <nickrod518@gmail.com>
2018-10-16 08:01:55 -0400

    Simplify entrypoint and have it accept mapped conf

--- start.sh
+++ start.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-CONF=/app/ngrok2.yml
-
-touch $CONF
-if [ ! -z "$NGROKTOKEN" ]; then
-	echo "authtoken: $NGROKTOKEN" >> $CONF
-fi
-echo 'inspect_addr: 0.0.0.0:4040' >> $CONF
-
 # Start sshd
 /usr/sbin/sshd &
 
 # Start ngrok
-ngrok tcp -config=$CONF 22
+ngrok start --all -config /app/ngrok.yml
